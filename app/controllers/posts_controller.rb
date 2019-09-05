67fa40e6740f7050fafa45802d9e88458dcbc28c
Blaise-gustave <uyisengablaise@gmail.com>
2019-09-05 17:18:18 +0000

    indentation correction

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -51,11 +51,12 @@ class PostsController < ApplicationController
   end
 
   private
+  
   def set_post
-  @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def post_params
-  params.require(:post).permit(:image, :image_cache, :content, :user_id)
+    params.require(:post).permit(:image, :image_cache, :content, :user_id)
   end
 end
